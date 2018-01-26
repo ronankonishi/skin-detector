220ba0b01810201ad9196d11d47961645715afb1
Ronan687 <ronankonishi16@gmail.com>
2018-01-25 18:48:33 -0800

    image display working FINALLY :)

--- src/main/java/com/research/skindetector/ImagePipeline.java
+++ src/main/java/com/research/skindetector/ImagePipeline.java
@@ -5,11 +5,8 @@ import org.datavec.api.writable.Writable;
 import org.datavec.api.io.labels.PathLabelGenerator;
 import org.datavec.api.io.labels.ParentPathLabelGenerator;
 import org.datavec.api.records.listener.impl.LogRecordListener;
-import org.datavec.api.records.reader.impl.jackson.FieldSelection;
-import org.datavec.api.records.reader.impl.jackson.JacksonRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.image.loader.NativeImageLoader;
-import org.datavec.image.recordreader.BaseImageRecordReader;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -51,7 +48,7 @@ public class ImagePipeline {
         int outputNum = 5;
         int numEpochs = 15;
 
-        File testing = new File("/Users/Ronan/Desktop/Testing/");
+//        File testing = new File("/Users/Ronan/Desktop/Testing/");
         //Define File Paths
         File trainData = new File("/Users/Ronan/ISIC-images/ISIC-images/UDA-1");
 //        File testData = new File("/Users/Ronan/ISIC-images/ISIC-images/UDA-2");
@@ -61,19 +58,11 @@ public class ImagePipeline {
 //        FileSplit test = new FileSplit(testData, NativeImageLoader.ALLOWED_FORMATS, ranNumGen);
 
         //creates label
-//        try {
-//            JsonReader jsonReader = Json.createReader(new FileReader("ISIC_0011403.json"));
-//            JsonObject json = jsonReader.readObject();
-//            String ID = json.getString("_id");
-//            String malignancy = json.getJsonObject("meta").getJsonObject("clinical").getString("benign_malignant");
-//        }
-//        catch (FileNotFoundException e){ e.printStackTrace();}
-//        catch (IOException e){ e.printStackTrace();}
-////		catch (ParseException e){ e.printStackTrace();}
-//        catch (Exception e){ e.printStackTrace();}
+//        JsonPathLabelGenerator label = new JsonPathLabelGenerator();
+        ParentPathLabelGenerator label = new ParentPathLabelGenerator();
 
         //rescale, converts, and labels images
-        BaseImageRecordReader recordReader = new BaseImageRecordReader(height, width, channels, label);
+        ImageRecordReader recordReader = new ImageRecordReader(height, width, channels, label);
         recordReader.initialize(train);
         recordReader.setListeners(new LogRecordListener());
 
@@ -84,64 +73,50 @@ public class ImagePipeline {
         scaler.fit(dataIter);
         dataIter.setPreProcessor(scaler);
 
+        for (int i = 0; i < 3; i++){
+            DataSet ds = dataIter.next();
+            System.out.println(ds);
+            System.out.println(dataIter.getLabels());
+        }
+
 // Build Our Neural Network
 
-        log.info("**** Build Model ****");
-
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(rngseed)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .iterations(1)
-                .learningRate(0.006)
-                .updater(Updater.NESTEROVS)
-                .regularization(true).l2(1e-4)
-                .list()
-                .layer(0, new DenseLayer.Builder()
-                        .nIn(height * width)
-                        .nOut(100)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nIn(100)
-                        .nOut(outputNum)
-                        .activation(Activation.SOFTMAX)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .pretrain(false).backprop(true)
-                .setInputType(InputType.convolutional(height,width,channels))
-                .build();
-
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
-
-        // The Score iteration Listener will log
-        // output to show how well the network is training
-        model.setListeners(new ScoreIterationListener(10));
-
-        log.info("*****TRAIN MODEL********");
-        for(int i = 0; i < numEpochs; i++){
-            model.fit(dataIter);
-        }
-    }
-
-    private static class LabelGen implements PathLabelGenerator {
-
-        public Writable getLabelForPath(String path) {
-            if (path.endsWith("0.txt"))
-                return new IntWritable(0);
-            else if (path.endsWith("1.txt"))
-                return new IntWritable(1);
-            else
-                return new IntWritable(2);
-        }
-
-        public Writable getLabelForPath(URI uri) {
-            return getLabelForPath(uri.getPath());
-        }
-
-        public boolean inferLabelClasses() {
-            return true;
-        }
+//        log.info("**** Build Model ****");
+//
+//        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+//                .seed(rngseed)
+//                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+//                .iterations(1)
+//                .learningRate(0.006)
+//                .updater(Updater.NESTEROVS)
+//                .regularization(true).l2(1e-4)
+//                .list()
+//                .layer(0, new DenseLayer.Builder()
+//                        .nIn(height * width)
+//                        .nOut(100)
+//                        .activation(Activation.RELU)
+//                        .weightInit(WeightInit.XAVIER)
+//                        .build())
+//                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+//                        .nIn(100)
+//                        .nOut(outputNum)
+//                        .activation(Activation.SOFTMAX)
+//                        .weightInit(WeightInit.XAVIER)
+//                        .build())
+//                .pretrain(false).backprop(true)
+//                .setInputType(InputType.convolutional(height,width,channels))
+//                .build();
+//
+//        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+//
+//        // The Score iteration Listener will log
+//        // output to show how well the network is training
+//        model.setListeners(new ScoreIterationListener(10));
+//
+//        log.info("*****TRAIN MODEL********");
+//        for(int i = 0; i < numEpochs; i++){
+//            model.fit(dataIter);
+//        }
     }
 }
 
--- src/main/java/com/research/skindetector/JsonPathLabelGenerator.java
+++ src/main/java/com/research/skindetector/JsonPathLabelGenerator.java
@@ -2,26 +2,58 @@ package com.research.skindetector;
 
 import org.datavec.api.io.labels.PathLabelGenerator;
 import org.datavec.api.writable.Writable;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import java.io.File;
 import java.net.URI;
+import org.datavec.api.writable.Text;
 public class JsonPathLabelGenerator implements PathLabelGenerator {
 
     public JsonPathLabelGenerator() {}
 
-    @Override
+//    @Override
     public Writable getLabelForPath(String path) {
-        return
+        return JsonExtraction(path);
     }
 
-    @Override
-    public Writable getLabelForPath(URI uri) {
-        return
+    public Writable getLabelForPath(URI uri){
+        return null;
     }
+//
+//    @Override
+//    public Writable getLabelForPath(URI uri) {
+//        return JsonExtraction(uri);
+//    }
 
-    @Override
+//    @Override
     public boolean inferLabelClasses(){
         return true;
     }
 
+    public Writable JsonExtraction(String path){
+       try {
+            JsonReader jsonReader = Json.createReader(new FileReader(path)); //path is file name
+            JsonObject json = jsonReader.readObject();
+            return new Text(json.getJsonObject("meta").getJsonObject("clinical").getString("benign_malignant"));
+       }
+       catch (FileNotFoundException e){ e.printStackTrace();}
+       catch (IOException e){ e.printStackTrace();}
+       catch (Exception e){ e.printStackTrace();}
+       return null;
+    }
+//    public Writable JsonExtraction(URI uri){
+//        try {
+//            JsonReader jsonReader = Json.createReader(new File(uri)); //path is file name
+//            JsonObject json = jsonReader.readObject();
+//            return new Text(json.getJsonObject("meta").getJsonObject("clinical").getString("benign_malignant"));
+//        }
+//        catch (FileNotFoundException e){ e.printStackTrace();}
+//        catch (IOException e){ e.printStackTrace();}
+//        catch (Exception e){ e.printStackTrace();}
+//        return null;
+//    }
 }
