82535d690ad0375c14b7fdaade9f2d40bf9ff638
Ronan Konishi <ronankonishi16@gmail.com>
2018-02-13 15:20:57 -0800

    Javadoc

--- src/main/java/com/research/skindetector/JsonImageRecordReader.java
+++ src/main/java/com/research/skindetector/JsonImageRecordReader.java
@@ -11,14 +11,37 @@ import java.io.*;
 import java.net.URI;
 import java.util.*;
 
+/**
+ * JSON Image Record Reader
+ *
+ * A formated dataset for JPG images and labels within a JSON file.
+ *
+ * @author Ronan Konishi
+ * @version 1.0
+ */
 public class JsonImageRecordReader extends BaseImageRecordReader {
 
+    /**
+     * Constructor
+     *
+     * @param height The height of image in pixels
+     * @param width The width of image in pixels
+     * @param channels The number of channels (e.g. 1 for grayscaled and 3 for RGB)
+     * @param labelGenerator Label (of either malignant or benign) for a given JPG image
+     */
     public JsonImageRecordReader(int height, int width, int channels, PathLabelGenerator labelGenerator){
         super(height, width, channels, labelGenerator);
     }
 
+    /**
+     * Initializes the image record reader by transforming input images and labeling them with their corresponding 
+     * status (either malignant or benign) in a format compatible by the deeplearning4j library.
+     * 
+     * @param split The file path for the dataset that should be initialized
+     */
     @Override
     public void initialize(InputSplit split) throws IOException {
+        //transforms image to the given height and width for the given channel
         if (imageLoader == null) {
             imageLoader = new NativeImageLoader(height, width, channels, imageTransform);
         }
