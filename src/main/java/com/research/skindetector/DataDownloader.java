49a42c14ed7820a9ce049794a6c26116f546ee2d
Ronan Konishi <ronankonishi16@gmail.com>
2018-02-15 13:24:37 -0800

    Update and rename DataDownloader to DataDownloader.java

---
+++ src/main/java/com/research/skindetector/DataDownloader.java
@@ -0,0 +1,31 @@
+package com.research.skindetector;
+
+import org.apache.commons.io.FilenameUtils;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.records.listener.impl.LogRecordListener;
+import org.datavec.api.split.FileSplit;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.examples.utilities.DataUtilities;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Random;
+
+/**
+ * Inspired by tom hanlon, who posted his code on deeplearning4j
+ * 
+ * 
+ */
+
+public class DataDownloader {
+  
+  
+  
+}
